8c6a9b5313fd719d7b7652ffe202d8d52944ed15
Stefan <stefan.armijo@gmail.com>
2018-08-11 20:21:06 -0700

    deleted erratum

--- sdfas.js
+++
@@ -1,6 +0,0 @@
-console.log(
-    lScript.reduce((a, b) => {
-        return charCount(a) < charCount(b) ? b : a;
-    }).ranges.reduce((a, c)
-        => a + c.reduce((a, c)
-            => c - a), 0));
